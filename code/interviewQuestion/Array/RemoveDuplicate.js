fe3a99ddf435430ac5f06f7561b6ba67deada6ae
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2024-05-14 22:02:42 +0530

    Updated

--- code/interviewQuestion/Array/RemoveDuplicate.js
+++ code/interviewQuestion/Array/RemoveDuplicate.js
@@ -11,3 +11,19 @@ var removeDuplicates = function (nums) {
   }
   console.log(updatedArray);
 };
+
+// Input: nums = [1,1,1,2,2,3]
+// Output: 5, nums = [1,1,2,2,3,_]
+// Here we can keep at most two duplicate number
+var removeDuplicates = function (nums) {
+  let k = 0;
+
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 2]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
