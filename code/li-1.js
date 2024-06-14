36cdd87edee279f49a796284b869f0eec478ad4b
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2024-06-14 16:48:08 +0530

    Two Sum problem

--- code/li-1.js
+++ code/li-1.js
@@ -16,3 +16,44 @@ var twoSum = function (nums, target) {
   }
   return resultArray;
 };
+//Input: nums = [2,7,11,15], target = 9 Output: [0,1]
+// Two Pinter Problems
+var twoSum = function (nums, target) {
+  let resultArray = [];
+  let start = 0; // Start and Increase the first index
+  let end = nums.length - 1; //// Start from last and decrease the last index
+  while (start < end) {
+    let sum = nums[start] + nums[end];
+    if (sum === target) {
+      console.log(start, end);
+      resultArray = [start, end];
+      break;
+    } else if (sum < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return resultArray;
+};
+
+// Same with for loop
+
+var twoSum = function (nums, target) {
+  let resultArray = [];
+  let start = 0;
+  let end = nums.length - 1;
+
+  for (; start < end; ) {
+    let sum = nums[start] + nums[end];
+    if (sum === target) {
+      resultArray = [start, end];
+      break;
+    } else if (sum < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return resultArray;
+};
