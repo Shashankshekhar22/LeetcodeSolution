854fdbe276e07849f94cb525e3bf38a568fced3f
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2024-08-10 16:56:10 +0530

    update code

--- code/interviewQuestion/Array/productExceptSelf.js
+++ code/interviewQuestion/Array/productExceptSelf.js
@@ -25,19 +25,24 @@ var productExceptSelf = function (nums) {
 
 var productExceptSelf = function (nums) {
   const n = nums.length;
-  const output = new Array(n);
-  console.log("o", output);
-  // Calculate left products
-  let leftProduct = 1;
+  const output = [];
+
+  const leftProducts = [];
+  const rightProducts = [];
+  leftProducts[0] = 1;
+  rightProducts[n - 1] = 1;
+
+  for (let i = 1; i < n; i++) {
+    leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+  }
+
+  for (let i = n - 2; i >= 0; i--) {
+    rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+  }
+
   for (let i = 0; i < n; i++) {
-    output[i] = leftProduct;
-    leftProduct *= nums[i];
+    output[i] = leftProducts[i] * rightProducts[i];
   }
-  // Calculate right products and multiply with left products
-  let rightProduct = 1;
-  for (let i = n - 1; i >= 0; i--) {
-    output[i] *= rightProduct;
-    rightProduct *= nums[i];
-  }
+
   return output;
 };
